ab44138997c3721653e8c4dc24e32bf486e0ce23
rnadrich <nadrichrobert@gmail.com>
2015-07-15 11:51:19 -0400

    Edited comments

--- Program.php
+++ Program.php
@@ -16,11 +16,10 @@ if(isset($_POST["submit"])) {
 }
 
 // Allow certain file formats
-/*if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
+    echo "Sorry, only JPG, JPEG, & PNG files are allowed.";
     $uploadOk = 0;
-}*/
+}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
