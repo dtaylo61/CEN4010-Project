6335daf2894e634ef38197b821bedf8296047cd5
rnadrich <nadrichrobert@gmail.com>
2015-07-30 08:03:09 -0400

    Match PHP style with other pages

--- Program.php
+++ Program.php
@@ -67,7 +67,7 @@ function processImage()
 			else {
 				echo "There are $output[0] $type in the Picture</br>
 				Total value: $" . $value*$output[0] . "</br>";
-				echo "<img src=\"$target_out_file\" alt=\"Results\"> </br>";
+				echo "<img src=\"$target_out_file\" alt=\"Results\" style=\"vertical-align: middle;width:304px;height:228px;\"> </br>";
 
 			}
 		}
@@ -79,21 +79,63 @@ function processImage()
 }
 
 echo '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Change Counter</title>
-        <link rel="stylesheet" href="css/base.css" type="text/css" media="screen">
-    </head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- <link rel="icon" href="images/cents.ico"> -->
+
+    <title>Change Counter</title>
+      
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/cc-cover.css" rel="stylesheet">
+
+  </head>
 
     <body>
+	 <div class="navbar">
+    <div class="container">
+        <!-- Collapse button -->
+        <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+            <span class="text">Show Menu</span>
+        </button>
+        <!-- Navigation buttons -->
+        <!-- Collapse content -->
+        <div class="collapse navbar-collapse navHeaderCollapse">
+                <ul class="nav navbar-nav navbar-left">					
+                    <li class="active">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu<span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="upload-main.html">Go to Upload</a></li>
+                            <li><a href="#">Go to Count Info</a></li>
+                            <li><a href="#">Image Log</a></li>
+                            <li><a href="currency-type.html">Currency Type</a></li>
+                            <li><a href="about.html">About</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </div>
+      <!-- Changer Counter Logo -->
+      <div class="row">
+          <p><a href="index.html"><img src="http://i.imgur.com/azo1zCc.png" style="width:250px;height:104px;top-padding:50px"/></a></p>
+          </div> 
 	<div class ="container" id="input">
-		<section class="main-content">
+	<div class="jumbotron" style="width:100%;height:100%;padding-top:3px">
+		<section class="main-content" style="color:#0099ff;text-align:left;margin:20px">
 		<h1> Results</h1>
 		<p>';
 		processImage();
 echo '</p>
 </section>
+</div>
+</div>
 		</body>
 		</html>';
 	
